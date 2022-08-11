c55e366cf3e64bd37b6c97b071013ef1d017c313
Jingtian Liu <liujt1205@gmail.com>
2022-08-10 23:13:22 -0500

    some structure

---
+++ Client/src/components/App.jsx
@@ -0,0 +1,4 @@
+const ProductOverview = lazy(() => import('./ProductOverview.jsx'));
+const QnA = lazy(() => import('./QnA.jsx'));
+const RelatedProducts = lazy(() => import('./RelatedProducts.jsx'));
+const Reviews = lazy(() => import('./Reviews.jsx'));
