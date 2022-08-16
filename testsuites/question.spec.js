c313f03f7b32a82b1c0f598bad05a19108f3d9e8
Alex Yeung <alexyeung@Alexs-MacBook-Air.local>
2022-08-15 18:53:52 -0700

    added some tests

--- testsuites/question.spec.js
+++ testsuites/question.spec.js
@@ -1,1 +1,15 @@
 // add test suite here
+const QnA = require("../Client/src/components/QnA.jsx");
+const TestRenderer = require('react-test-renderer');
+
+describe("QnA", function(){
+  test("Initial Hello World", function(){
+
+    const testRenderer = TestRenderer.create(<QnA></QnA>);
+    console.log(testRenderer);
+
+    // It's possible to have multiple expects in a single test like this. However, it is often unhelpful.
+    // Just write two tests referring to the "Example Suites" example above for reference.
+    expect(testRenderer).toBe('<p>Hello world!</p>');
+  });
+});
