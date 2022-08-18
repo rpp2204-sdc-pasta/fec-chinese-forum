2586c22a3f0dce7668853e97073557e14b72f676
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2022-08-17 21:42:24 -0500

    Update App.jsx

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -1,9 +1,8 @@
 import { lazy } from 'react';
-// const ProductOverview = lazy(() => import('./ProductOverview.jsx'));
-// const QnA = lazy(() => import('./QnA.jsx'));
-// const RelatedProducts = lazy(() => import('./RelatedProducts.jsx'));
-// const Reviews = lazy(() => import('./Reviews.jsx'));
-import Reviews from './Reviews.jsx'
+const ProductOverview = lazy(() => import('./ProductOverview.jsx'));
+const QnA = lazy(() => import('./QnA.jsx'));
+const RelatedProducts = lazy(() => import('./RelatedProducts.jsx'));
+const Reviews = lazy(() => import('./Reviews.jsx'));
 
 
 let App = (props) => (
@@ -11,9 +10,9 @@ let App = (props) => (
     <div>
       some title and nev bar maybe?
     </div>
-    {/* <ProductOverview /> */}
-    {/* <RelatedProducts /> */}
-    {/* <QnA /> */}
+    <ProductOverview />
+    <RelatedProducts />
+    <QnA />
     <Reviews />
   </>
 )
