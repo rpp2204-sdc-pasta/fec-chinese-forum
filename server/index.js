7999cc2f1ee8f6dbf4a76b84a4c1cbe6297c148d
Jingtian Liu <liujt1205@gmail.com>
2022-08-17 22:30:13 -0500

    implemented one route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
+const axios = require('axios');
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const { getRelated } = require('./related');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
 
+app.get('/related/:id', (req, res) => {
+  getRelated(req, res);
+});
+
+app.get('/outfit', (req, res) => {
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
