f29e2270fd14078fcffa5ab679de766335538174
Jingtian Liu <liujt1205@gmail.com>
2022-08-18 00:04:46 -0500

    improved getRelated

--- server/related.js
+++ server/related.js
@@ -15,23 +15,36 @@ exports.getRelated = (req, res) => {
       let moreAPICalls = response.data.map(num => {
         let options = {
           method: 'GET',
+          url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${num}`,
+          headers: {
+            Authorization: process.env.AUTH
+          }
+        };
+        let options2 = {
+          method: 'GET',
           url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${num}/styles`,
           headers: {
             Authorization: process.env.AUTH
           }
-        }
-        return axios(options);
-      })
+        };
+        let options3 = {
+          method: 'GET',
+          url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews/meta/?product_id=${num}`,
+          headers: {
+            Authorization: process.env.AUTH
+          }
+        };
+        return Promise.all([axios(options).then(response => response.data),
+          axios(options2).then(response => response.data),
+          axios(options3).then(response => response.data)]);
+      });
       return Promise.all(moreAPICalls)
         .then(values => {
-          let result = values.map(value => {
-            // console.log(value.data.results[0].name);
+          let results = values.map(value => {
             let product = {};
-            value.data.results.forEach(style => {
+            value[1].results.forEach(style => {
               if (style['default?']) {
-                // console.log(style.name);
                 product = {
-                  product_id: value.data.product_id,
                   style_id: style.style_id,
                   original_price: style.original_price,
                   sale_price: style.sale_price,
@@ -39,10 +52,26 @@ exports.getRelated = (req, res) => {
                 }
               }
             });
+            let oneStars = value[2].ratings['1'] || 0;
+            let twoStars = value[2].ratings['2'] || 0;
+            let threeStars = value[2].ratings['3'] || 0;
+            let fourStars = value[2].ratings['4'] || 0;
+            let fiveStars = value[2].ratings['5'] || 0;
+            let total = (1 * oneStars) + (2 * twoStars) + (3 * threeStars) + (4 * fourStars)
+            + (5 * fiveStars);
+            let totalReviews = Number(oneStars) + Number(twoStars) + Number(threeStars) + Number(fourStars) + Number(fiveStars);
+            let overallRating = Math.round(100 * total / totalReviews) / 100;
+            product = {
+              ...product,
+              id: value[0].id,
+              name: value[0].name,
+              category: value[0].category,
+              overallRating
+            }
             return product;
-          })
-          // console.log(result);
-          return result;
+          });
+          return results;
         });
     });
 }
+
