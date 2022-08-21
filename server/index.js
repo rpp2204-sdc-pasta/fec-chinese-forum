db5eb690524762e6bc952bf5bc320562454d45e7
Zefeng <69022141+zzsocool@users.noreply.github.com>
2022-08-21 16:07:43 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ const app = express();
 const reviews =require('./reviews.js')
 const port = process.env.PORT || 3000;
 const { getRelated } = require('./related');
+const { Outfit } = require('../db/index.js');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 
+//https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp
 
 app.get('/related/:id', (req, res) => {
   getRelated(req.params.id)
@@ -23,7 +25,39 @@ app.get('/related/:id', (req, res) => {
 });
 
 app.get('/outfit', (req, res) => {
+  Outfit.find()
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("some err happened");
+    });
+});
 
+app.post('/outfit', (req, res) => {
+  const { id, category, name, original_price, sale_price, img_url, overallRating } = req.body;
+  const obj = { id, category, name, original_price, sale_price, img_url, overallRating };
+  Outfit.updateOne({id: id}, obj, {upsert: true}, function(err) {
+    if (err) {
+      console.log(err)
+      res.status(400);
+    } else {
+      res.status(201);
+    }
+  });
+});
+
+app.delete('/outfit', (req, res) => {
+  const { id } = req.body;
+  Outfit.deleteOne({id: id}, function(err) {
+    if (err) {
+      console.log(err)
+      res.status(406);
+    } else {
+      res.status(204);
+    }
+  })
 });
 
 //===========================================
