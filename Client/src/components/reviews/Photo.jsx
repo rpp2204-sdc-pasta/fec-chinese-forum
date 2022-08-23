fa070027c97cfee96ce797bede8d4d915b508630
Zefeng Shen <zefengshen@Zefengs-MacBook-Pro.local>
2022-08-22 19:27:04 -0700

    image css wip

--- Client/src/components/reviews/Photo.jsx
+++ Client/src/components/reviews/Photo.jsx
@@ -3,17 +3,25 @@ import {useState} from 'react'
 
 
 const Photo = (props)=>{
-  const style = {display: none,
-  position: fixed,
-  zIndex: 1,
-  paddingTop: 100px,
-  left: 0,
-  top: 0,
-  width: 100%,
-  height: 100%,
-  overFlow: auto,
-  backgroundColor: rgb(0,0,0),
-  backgroundColor: rgba(0,0,0,0.9),
+  const container ={
+    display: 'none',
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    background: 'rgba(90,90,90, 0.5)',
+    zIndex: 9999,
+  }
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    border: '5px solid #fff',
+    width: '750px',
+    borderRadius: '5px',
+    objectFit: 'cover',
 }
   const [modal, setModal] =useState(true);
 
@@ -24,20 +32,44 @@ const Photo = (props)=>{
 
   if (modal){
     return(
+      <div>
         <img onClick={toggleModal} src={props.photo.url}
         height= '60'
         width='10%'
         alt={props.photo.id}/>
+      </div>
 
     )
   } else {
     return (
-      <img onClick={toggleModal} src={props.photo.url} className='modalbackground'
-      style={style}
-      alt={props.photo.id}/>
+      <div>
+        <img onClick={toggleModal} src={props.photo.url} className='modalbackground'
+        style={style}
+        alt={props.photo.id}/>
+
+      </div>
     )
   }
+      // return(
+      // <div>
+      //   <img onClick={toggleModal} src={props.photo.url}
+      //   height= '50'
+      //   width='5%'
+      //   alt={props.photo.id}/>
+      //   {modal && (
+      //     <dialog className='modalbackground'
+      //     style={style}
+      //     onClick={toggleModal} >
+      //       <img className='image'
+      //       src={props.photo.url}
+      //       onClick={toggleModal}
+      //       alt={props.phot.id}
+      //       />
+      //     </dialog>
+      //   )}
+      // </div>
 
+    // )
 }
 
 
