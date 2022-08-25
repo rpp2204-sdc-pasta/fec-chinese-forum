3c2591c58d833659209017a7402435b8e58b7ec7
Zefeng Shen <zefengshen@Zefengs-MacBook-Pro.local>
2022-08-24 18:59:05 -0700

    css webpack

--- Client/src/components/reviews/Photo.jsx
+++ Client/src/components/reviews/Photo.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import {useState} from 'react'
+import '/Users/zefengshen/Documents/hack reactor/chinese_forum/Client/src/styles/style.css'
+// import '..../src/styles/style.css';
 
 
 const Photo = (props)=>{
-  const container ={
-    display: 'none',
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    background: 'rgba(90,90,90, 0.5)',
-    zIndex: 9999,
-  }
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    border: '5px solid #fff',
-    width: '750px',
-    borderRadius: '5px',
-    objectFit: 'cover',
-}
+//   const container ={
+//     display: 'none',
+//     position: 'fixed',
+//     top: 0,
+//     bottom: 0,
+//     left: 0,
+//     right: 0,
+//     background: 'rgba(90,90,90, 0.5)',
+//     zIndex: 9999,
+//   }
+//   const style = {
+//     position: 'absolute',
+//     top: '50%',
+//     left: '50%',
+//     transform: 'translate(-50%, -50%)',
+//     border: '5px solid #fff',
+//     width: '750px',
+//     borderRadius: '5px',
+//     objectFit: 'cover',
+// }
   const [modal, setModal] =useState(true);
 
   const toggleModal= () =>{
@@ -32,14 +34,19 @@ const Photo = (props)=>{
 
 
     return(
-      <div className='modal' id='modal'>
-
+      <div>
         <img onClick={toggleModal} src={props.photo.url}
-        height= '60'
-        width='10%'
-        alt={props.photo.id}/>
+          height= '60'
+          width='10%'
+          alt={props.photo.id}/>
+        <div className='modal' id='modal'>
+          <div className='modal-header'>
+          <img onClick={toggleModal} src={props.photo.url}
+          alt={props.photo.id}/>
+          </div>
+        </div>
+        <div id='overlay'></div>
       </div>
-
     )
       // return(
       // <div>
