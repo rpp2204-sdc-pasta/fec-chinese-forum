fe04a07b4c6395bef28e4f08218e60440e2d628d
Zefeng Shen <zefengshen@Zefengs-MacBook-Pro.local>
2022-08-25 19:45:47 -0700

    image css completed

--- Client/src/components/reviews/Photo.jsx
+++ Client/src/components/reviews/Photo.jsx
@@ -1,73 +1,62 @@
 import React from 'react';
 import {useState} from 'react'
-import '/Users/zefengshen/Documents/hack reactor/chinese_forum/Client/src/styles/style.css'
+import css from '/Users/zefengshen/Documents/hack reactor/chinese_forum/Client/src/styles/style.css'
 // import '..../src/styles/style.css';
 
+const imagestyle = {
+  objectFit: 'cover',
+  border: '0.5px solid #ddd',
+  borderRadius: '50%',
+  // padding: '5px',
+  height: '30px',
+  width: '30px',
+  position: 'relative',
+  zInder: 1
+}
+const another_style ={
+  position:'fixed',
+  marginLetf: 'auto',
+  marginRight: 'auto',
+  height: 'auto',
+  width: 'auto',
+  maxWidth: '800px',
+  maxHeight: '800px',
+  top: '50%',
+  left:'50%',
+  transform: 'translate(-50%,-50%)',
+  backgroundColor: '#FFF',
+  padding: '5px',
+  zInder: 1000
+}
+
+const overlay = {
+  position:'fixed',
+  top:0,
+  left:0,
+  right:0,
+  bottom:0,
+  backgroundColor: 'rgba(0,0,0, .7)',
+  zIndex: 1000
+}
 
 const Photo = (props)=>{
-//   const container ={
-//     display: 'none',
-//     position: 'fixed',
-//     top: 0,
-//     bottom: 0,
-//     left: 0,
-//     right: 0,
-//     background: 'rgba(90,90,90, 0.5)',
-//     zIndex: 9999,
-//   }
-//   const style = {
-//     position: 'absolute',
-//     top: '50%',
-//     left: '50%',
-//     transform: 'translate(-50%, -50%)',
-//     border: '5px solid #fff',
-//     width: '750px',
-//     borderRadius: '5px',
-//     objectFit: 'cover',
-// }
-  const [modal, setModal] =useState(true);
+
+  const [isOpen, setIsOpen] =useState(false);
 
   const toggleModal= () =>{
     console.log('modal')
-    setModal (!modal)
+    setIsOpen (!isOpen)
   }
 
-
     return(
-      <div>
-        <img onClick={toggleModal} src={props.photo.url}
-          height= '60'
-          width='10%'
-          alt={props.photo.id}/>
-        <div className='modal' id='modal'>
-          <div className='modal-header'>
-          <img onClick={toggleModal} src={props.photo.url}
-          alt={props.photo.id}/>
-          </div>
-        </div>
-        <div id='overlay'></div>
+    <>
+      <div   className={props.photo.id}>
+         <img  style={imagestyle} onClick={toggleModal} src={props.photo.url} alt={props.photo.id}></img>
+         {isOpen && <div style={overlay}><img  style={another_style} onClick={toggleModal} src={props.photo.url} alt={props.photo.id}></img></div>}
       </div>
-    )
-      // return(
-      // <div>
-      //   <img onClick={toggleModal} src={props.photo.url}
-      //   height= '50'
-      //   width='5%'
-      //   alt={props.photo.id}/>
-      //   {modal && (
-      //     <dialog className='modalbackground'
-      //     style={style}
-      //     onClick={toggleModal} >
-      //       <img className='image'
-      //       src={props.photo.url}
-      //       onClick={toggleModal}
-      //       alt={props.phot.id}
-      //       />
-      //     </dialog>
-      //   )}
-      // </div>
 
-    // )
+    </>
+    )
 }
 
 
