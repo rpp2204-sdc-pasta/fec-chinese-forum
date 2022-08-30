82976dcc796cf8d128a7c1471c1503160b762e73
dchiu91 <d.chiu91@yahoo.com>
2022-08-29 22:48:00 -0700

    revert comments

--- db/index.js
+++ db/index.js
@@ -1,18 +1,18 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/fetcher');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fetcher');
 
-// let outfitSchema = mongoose.Schema({
-//   id: Number,
-//   category: String,
-//   name: String,
-//   original_price: Number,
-//   sale_price: Number,
-//   img_url: String,
-//   overallRating: Number
-// });
+let outfitSchema = mongoose.Schema({
+  id: Number,
+  category: String,
+  name: String,
+  original_price: Number,
+  sale_price: Number,
+  img_url: String,
+  overallRating: Number
+});
 
-// let Outfit = mongoose.model('Outfit', outfitSchema);
+let Outfit = mongoose.model('Outfit', outfitSchema);
 
 
 
-// module.exports.Outfit = Outfit;
+module.exports.Outfit = Outfit;
