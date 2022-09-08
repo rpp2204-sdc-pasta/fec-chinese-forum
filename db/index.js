eeec315377f1f2dcffc3ead5552de5a5dc9a56d6
dchiu91 <d.chiu91@yahoo.com>
2022-09-07 22:02:15 -0700

    add some css and fixed some feature bugs

--- db/index.js
+++ db/index.js
@@ -1,19 +1,19 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/fetcher');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fetcher');
 
-// let outfitSchema = mongoose.Schema({
-//   id: Number,
-//   category: String,
-//   name: String,
-//   original_price: String,
-//   sale_price: String,
-//   img_url: String,
-//   overallRating: Number,
-//   reviewCount: Number
-// });
+let outfitSchema = mongoose.Schema({
+  id: Number,
+  category: String,
+  name: String,
+  original_price: String,
+  sale_price: String,
+  img_url: String,
+  overallRating: Number,
+  reviewCount: Number
+});
 
-// let Outfit = mongoose.model('Outfit', outfitSchema);
+let Outfit = mongoose.model('Outfit', outfitSchema);
 
 
 
-// module.exports.Outfit = Outfit;
+module.exports.Outfit = Outfit;
