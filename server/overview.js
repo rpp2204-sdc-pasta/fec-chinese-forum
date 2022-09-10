9b13ddfa534cbda238d5519632ac3af5fd6e29dc
dchiu91 <d.chiu91@yahoo.com>
2022-09-10 10:34:16 -0700

    fix extra error handling

--- server/overview.js
+++ server/overview.js
@@ -3,27 +3,23 @@ const axios = require('axios');
 const { getProductInfo, getProductStyles, getProductReviewRating, getOverAllRating } = require('./related');
 
 let getOverview = (id) => {
-  try {
-    return Promise.all([getProductInfo(id), getProductStyles(id), getProductReviewRating(id)])
-      .then( (responses) => {
-        var overviewData;
-        overviewData=responses[0].data;
-        overviewData.styles=responses[1].data.results;
 
-        let sum = 0;
-        let count = 0;
-        let reviewsMeta = responses[2].data.ratings;
-        [overviewData.starRating, overviewData.reviewCount] = getOverAllRating(reviewsMeta);
-        return overviewData;
-      })
-  } catch {
-    console.log('error getting overview')
-  }
+  return Promise.all([getProductInfo(id), getProductStyles(id), getProductReviewRating(id)])
+    .then( (responses) => {
+      var overviewData;
+      overviewData=responses[0].data;
+      overviewData.styles=responses[1].data.results;
+
+      let sum = 0;
+      let count = 0;
+      let reviewsMeta = responses[2].data.ratings;
+      [overviewData.starRating, overviewData.reviewCount] = getOverAllRating(reviewsMeta);
+      return overviewData;
+    })
 
 };
 
 let addToCart = (sku, quantity) => {
-  try {
     let option = {
       method: 'POST',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/cart`,
@@ -38,9 +34,6 @@ let addToCart = (sku, quantity) => {
         // console.log(res.data);
         return res.data;
       })
-  } catch {
-    console.log('error add to cart');
-  }
 };
 
 module.exports.addToCart = addToCart;
