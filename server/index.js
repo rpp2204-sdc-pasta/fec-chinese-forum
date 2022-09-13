e4ccd3973a9d3ba0fd21d2275d9a874eba94b2c7
David Chiu <42689526+dchiu91@users.noreply.github.com>
2022-09-12 21:43:38 -0700

    Update index.js
    remove setting 0 quantity in stock for dummy test SKU

--- server/index.js
+++ server/index.js
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, '../Client/dist')));
 app.get('/overview/:id', (req, res) => {
   getOverview(req.params.id)
     .then(result => {
-      result.styles[0].skus[2580773].quantity= 0;
-      console.log(result.styles[0].skus[2580773]);
       res.status(200).send(result);
     })
     .catch(err => {
