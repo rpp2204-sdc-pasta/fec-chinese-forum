e13a1ff8856e062cccad473aa9d70e0c3d978d3f
David Chiu <42689526+dchiu91@users.noreply.github.com>
2022-09-14 10:07:58 -0700

    Update OverviewGallery.jsx

--- Client/src/components/ProductOverviewComponents/OverviewGallery.jsx
+++ Client/src/components/ProductOverviewComponents/OverviewGallery.jsx
@@ -19,7 +19,7 @@ class OverviewGallery extends React.Component {
       };
 
     };
-    let origPhotoUrl = this.props.photos[this.props.mainImageIndex].url
+    let origPhotoUrl = this.props.photos[this.props.mainImageIndex].url;
     let smallerPhotoUrl = origPhotoUrl.slice(0,-4)+'q=10';
     return (
       <div className='Overview-gallery'>
