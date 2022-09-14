fa9482292168dd34171577819c47ed00215bd45a
dchiu91 <d.chiu91@yahoo.com>
2022-09-14 10:05:37 -0700

    fix url typo

--- Client/src/components/ProductOverviewComponents/OverviewGallery.jsx
+++ Client/src/components/ProductOverviewComponents/OverviewGallery.jsx
@@ -19,8 +19,9 @@ class OverviewGallery extends React.Component {
       };
 
     };
-    let origPhotoUrl = this.props.photos[this.props.mainImageIndex].url
-    let smallerPhotoUrl = origPhotoUrl.slice(0,-4)+'?=10';
+    let origPhotoUrl = this.props.photos[this.props.mainImageIndex].url;
+    let smallerPhotoUrl = origPhotoUrl.slice(0,-4)+'q=10';
+    console.log(origPhotoUrl, smallerPhotoUrl);
     return (
       <div className='Overview-gallery'>
         <OverviewGalleryMain
